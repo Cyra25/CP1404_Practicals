ce302814ba915c4d4dba00313df7258811e9ff11
Chaw Su Thaw Tar <chawsuthawtar@my.jcu.edu.au>
2018-11-20 15:20:14 +0800

    Practical files

--- Prac_03/task1.py
+++ Prac_03/task1.py
@@ -1,5 +1,15 @@
-name = input("your name?")
-while len(name)< 1:
-    print("Invalid")
-for i in range(0,len(name),2):
-    print(i)
+def main():
+    name = get_name()
+    for i in range(0,len(name),2):
+        print(i)
+
+
+def get_name():
+    name = input("your name?")
+    while len(name) < 1:
+        print("Invalid")
+        name = input("your name?")
+    return name
+
+
+main()
