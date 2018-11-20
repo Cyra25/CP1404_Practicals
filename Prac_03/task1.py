11b2f4d7362fbc5150d44257deeaf946fa770e9e
Chaw Su Thaw Tar <chawsuthawtar@my.jcu.edu.au>
2018-11-20 20:55:23 +0800

    Practical files

--- Prac_03/task1.py
+++ Prac_03/task1.py
@@ -1,7 +1,17 @@
 def main():
     name = get_name()
-    for i in range(0,len(name),2):
-        print(i)
+    num = get_frequency()
+
+    for i in range(0, len(name), num):
+        print(name[i])
+
+
+def get_frequency():
+    num = input("What is the frequency of the letter?")
+    while num.isdigit() != False:
+        print("Invalid, not digit")
+
+
 
 def get_name():
     name = input("your name?")
