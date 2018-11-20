c1fa602f735da439da5dac3cd409c068fc343d4d
Chaw Su Thaw Tar <chawsuthawtar@my.jcu.edu.au>
2018-11-20 21:35:51 +0800

    Practical files

--- Prac_03/Debugging_from_P1.py
+++ Prac_03/Debugging_from_P1.py
@@ -4,17 +4,31 @@ Broken program to determine score status
 """
 
 # TODO: Fix this!
-
-score = float(input("Enter score: "))
-
-if score <0 or score > 100:
-    print("Invalid score")
-else:
-    if score >= 90:
-        print("Excellent")
-    elif score >=50:
-        print("Pass")
+def main():
+    score = float(input("Enter score: "))
+    result = take_score(score)
+    if result == 4:
+        print("Invalid score")
     else:
-        print("Fail")
+        if result == "1":
+            print("Excellent")
+        elif result == "2":
+            print("Pass")
+        else:
+            print("Fail")
 
-print("thanks")
+    print("thanks")
+
+def take_score(score):
+    if score < 0 or score > 100:
+        result = "4"
+    else:
+        if score >= 90:
+            result = "1"
+        elif score >= 50:
+            result = "2"
+        else:
+            result = "3"
+    return result
+
+main()
