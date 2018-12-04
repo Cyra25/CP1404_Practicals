5a407a881f0581ec24ef218a9cf65183515b6029
Chaw Su Thaw Tar <chawsuthawtar@my.jcu.edu.au>
2018-12-04 14:49:31 +0800

    add the error checking for something which is not in the list

--- Prac_05/color_hex.py
+++ Prac_05/color_hex.py
@@ -1,15 +1,17 @@
-HEXA_CODE = {'AliceBlue':'#f0f8ff',
-             'AntiqueWhite':'#faebd7',
-             'aquamarine1':'#7fffd4',
-             'beige':'#f5f5dc',
+HEXA_CODE = {'AliceBlue': '#f0f8ff',
+             'AntiqueWhite': '#faebd7',
+             'aquamarine1': '#7fffd4',
+             'beige': '#f5f5dc',
              'black': '#000000',
              'brown': '#a52a2a',
              'burlywood': '#deb887',
-             'CadetBlue': '	#5f9ea0',
-             'chocolate': '	#d2691e',
+             'CadetBlue': '#5f9ea0',
+             'chocolate': '#d2691e',
              'coral': '#ff7f50'}
 
 
 user_inp = input("Enter color name")
-
-print(HEXA_CODE[user_inp])
+if user_inp in HEXA_CODE:
+    print(HEXA_CODE[user_inp])
+else:
+    print("Not in the list")
