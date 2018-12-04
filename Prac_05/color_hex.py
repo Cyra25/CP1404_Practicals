7b4cef780bee77c496653e00f720a769bc17ccd1
Chaw Su Thaw Tar <chawsuthawtar@my.jcu.edu.au>
2018-12-04 14:36:05 +0800

    color hex, occurences and walkthrough

---
+++ Prac_05/color_hex.py
@@ -0,0 +1,15 @@
+HEXA_CODE = {'AliceBlue':'#f0f8ff',
+             'AntiqueWhite':'#faebd7',
+             'aquamarine1':'#7fffd4',
+             'beige':'#f5f5dc',
+             'black': '#000000',
+             'brown': '#a52a2a',
+             'burlywood': '#deb887',
+             'CadetBlue': '	#5f9ea0',
+             'chocolate': '	#d2691e',
+             'coral': '#ff7f50'}
+
+
+user_inp = input("Enter color name")
+
+print(HEXA_CODE[user_inp])
