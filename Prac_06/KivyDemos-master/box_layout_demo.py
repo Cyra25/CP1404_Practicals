cf96868460f035fa77741657a165f461ea10f231
Cyra25 <44994077+Cyra25@users.noreply.github.com>
2018-12-18 14:13:37 +0800

    box greet and clear

--- Prac_06/KivyDemos-master/box_layout_demo.py
+++ Prac_06/KivyDemos-master/box_layout_demo.py
@@ -9,4 +9,8 @@ class BoxLayoutDemo(App):
         return self.root
 
 
+def handle_greet(self):
+    self.root.ids.output_label.text = "Hello" + self.root.ids.input_name.text
+
+
 BoxLayoutDemo().run()
